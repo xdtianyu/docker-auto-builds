9ecf103534e3754e96441cc29a905d763efe48d5
tianyu <xdtianyu@gmail.com>
2017-01-10 23:51:32 +0800

    fix url

--- backend/eve/app.py
+++ backend/eve/app.py
@@ -1,14 +1,20 @@
 from eve import Eve
+from flask import current_app as app
 
+
+public = {
+    'allow_unknown': True,
+    'resource_methods': ['GET', 'POST']
+}
 
 config = {
     'MONGO_HOST': 'localhost',
     'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'contacts_db',
+    'MONGO_DBNAME': 'test',
     'URL_PREFIX': 'api',
     'API_VERSION': 'v1',
-    'DEBUG': True,
-    'DOMAIN': {'contacts': {}}
+    'DEBUG': False,
+    'DOMAIN': {"contacts": public}
 }
 
 
