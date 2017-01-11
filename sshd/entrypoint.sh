c2c94faea7e8f3a499f132c765801549fca44d55
tianyu <xdtianyu@gmail.com>
2017-01-11 20:14:20 +0800

    add sshd

---
+++ sshd/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+    # generate fresh rsa key
+    ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+    # generate fresh dsa key
+    ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+  mkdir -p /var/run/sshd
+fi
+
+/usr/sbin/nginx -g 'daemon on; master_process on;'
+
+supervisord -c /etc/supervisord.conf
+
+exec "$@"
