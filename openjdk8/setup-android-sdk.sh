f4b812dd94abd4c9a186818914afd494dc3dbed9
tianyu <xdtianyu@gmail.com>
2018-04-30 13:53:20 +0800

    update sdk license

--- openjdk8/setup-android-sdk.sh
+++ openjdk8/setup-android-sdk.sh
@@ -6,6 +6,7 @@ if [ ! -d "/opt/sdk" ];then
     unzip -qq tools_r25.2.3-linux.zip
     mkdir licenses
     echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > licenses/android-sdk-license
+    echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> licenses/android-sdk-license
     echo "84831b9409646a918e30573bab4c9c91346d8abd" > licenses/android-sdk-preview-license
 
     cd -
