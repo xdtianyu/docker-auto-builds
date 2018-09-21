bd70997dd75b7fe3077f24c53a002815baa421b2
tianyu <xdtianyu@gmail.com>
2018-09-22 00:01:58 +0800

    update openshift

--- openshift/entrypoint.sh
+++ openshift/entrypoint.sh
@@ -15,9 +15,19 @@ fi
 if [ ! -d "/var/run/sshd" ]; then
   mkdir -p /var/run/sshd
 fi
+if [ ! -d "/run/apache2" ]; then
+  mkdir -p /run/apache2
+fi
 
 if [ ! -z "$PORT" ]; then
     sed -i "s/8080/$PORT/g" /etc/nginx/conf.d/ttyd.conf
+fi
+
+if [ ! -z "$A_PORT" ]; then
+    sed -i "s/8081/$A_PORT/g" /etc/apache2/conf.d/ttyd.conf
+    sed -i "s/Listen\ 80/Listen\ $A_PORT/g" /etc/apache2/httpd.conf
+else
+    sed -i "s/Listen\ 80/Listen\ 8081/g" /etc/apache2/httpd.conf
 fi
 
 if [ ! -z "$SS_PORT" ]; then
@@ -27,6 +37,8 @@ fi
 if [ ! -z "$SS_PASS" ]; then
     sed -i "s/HqsHUPUfmnHYLJ9oIW/$SS_PASS/g" /etc/shadowsocks.json
 fi
+
+apachectl start
 
 supervisord -c /etc/supervisord.conf
 
