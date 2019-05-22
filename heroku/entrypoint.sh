418e83065b38d70d08bea0e202549a546619093f
tianyu <xdtianyu@gmail.com>
2019-05-23 00:33:50 +0800

    add ss v2ray

--- heroku/entrypoint.sh
+++ heroku/entrypoint.sh
@@ -16,6 +16,17 @@ fi
 
 sed -i "s/8080/$PORT/g" /etc/nginx/conf.d/ttyd.conf
 
+sed -i "s/RAY_PATH/$RAY_PATH/g" /etc/v2ray/config.json
+sed -i "s/RAY_PATH/$RAY_ID/g" /etc/v2ray/config.json
+sed -i "s/RAY_PATH/$RAY_PATH/g" /etc/nginx/conf.d/ttyd.conf
+
+sed -i "s/SS_PATH/$SS_PATH/g" /etc/supervisor.d/ss.ini
+sed -i "s/SS_PASS/$SS_PASS/g" /etc/supervisor.d/ss.ini
+sed -i "s/SS_PATH/$SS_PATH/g" /etc/nginx/conf.d/ttyd.conf
+
+
+rm /etc/nginx/conf.d/default.conf
+
 /usr/sbin/nginx -g 'daemon on; master_process on;'
 
 supervisord -c /etc/supervisord.conf
